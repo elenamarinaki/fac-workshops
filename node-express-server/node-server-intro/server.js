e7ae86ceb6aee8a50ba307e17a67e2f9e2ff3d99
Elena <rhapsodyenmauve@gmail.com>
2021-09-27 16:14:17 +0100

    static-files, post requests, request-body

--- node-express-server/node-server-intro/server.js
+++ node-express-server/node-server-intro/server.js
@@ -34,13 +34,38 @@ server.get('/users/:name', (request, response) => {
 //   response.status(404).send('<h1>Not found</h1>');
 // });
 
-server.get('/', (request, response, next) => {
+// server.get('/', (request, response, next) => {
+//   console.log(request.method + ' ' + request.url);
+//   next();
+// });
+
+// server.get('/', (request, response) => {
+//   response.send('<h1>Hello</h1>');
+// });
+
+function logger(request, response, next) {
   console.log(request.method + ' ' + request.url);
   next();
+}
+
+server.get('/', logger, (request, response) => {
+  response.send('<h1>Hello</h1>');
 });
 
-server.get('/', (request, response) => {
-  response.send('<h1>Hello</h1>');
+const staticHandler = express.static('public');
+
+server.use(staticHandler);
+
+// server.post('/submit', (request, response) => {
+//   console.log('posted');
+//   response.send('thanks for submitting');
+// });
+
+const bodyParser = express.urlencoded();
+
+server.post('/submit', bodyParser, (request, response) => {
+  console.log(request.body);
+  response.send('thanks for submitting');
 });
 
 const PORT = 3000;
