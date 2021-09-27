f659dd8106dfc20327ea76718449ba9ad80b9e9d
Elena <rhapsodyenmauve@gmail.com>
2021-09-27 15:37:53 +0100

    missing routes & middleware

--- node-express-server/node-server-intro/server.js
+++ node-express-server/node-server-intro/server.js
@@ -11,10 +11,10 @@ const server = express();
 //   response.set('header', 'using html tags').send('<h1>Hi all!</h1>');
 // });
 
-server.get('/', (request, response) => {
-  const time = new Date().toLocaleTimeString();
-  response.send(`<h1>Hello, it's ${time}</h1>`);
-});
+// server.get('/', (request, response) => {
+//   const time = new Date().toLocaleTimeString();
+//   response.send(`<h1>Hello, it's ${time}</h1>`);
+// });
 
 server.get('/redirects', (request, response) => {
   response.redirect('/json');
@@ -28,6 +28,19 @@ server.get('/json', (request, response) => {
 server.get('/users/:name', (request, response) => {
   const name = request.params.name;
   response.send(`<h1>Hello ${name}</h1>`);
+});
+
+// server.use((request, response) => {
+//   response.status(404).send('<h1>Not found</h1>');
+// });
+
+server.get('/', (request, response, next) => {
+  console.log(request.method + ' ' + request.url);
+  next();
+});
+
+server.get('/', (request, response) => {
+  response.send('<h1>Hello</h1>');
 });
 
 const PORT = 3000;
