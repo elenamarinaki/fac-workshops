4136ebe8940211084081fb3a9670f3ea1cd93faf
Elena <rhapsodyenmauve@gmail.com>
2021-09-27 15:03:14 +0100

    setting headers

--- node-express-server/node-server-intro/server.js
+++ node-express-server/node-server-intro/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const server = express();
 
 server.get('/', (request, response) => {
-  response.send('hello');
+  // response.send('hello').status(404);
+  // response.status(404).send('hello');
+  response.set({'header': 'greeting Danilo', 'header_2': 'test header'}).send('hi Danilo!');
 });
 
 const PORT = 3000;
