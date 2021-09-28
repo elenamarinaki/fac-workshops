bd36246f9edb0f5b4e2e9408c4a51c51797bfc2a
Elena <rhapsodyenmauve@gmail.com>
2021-09-28 22:10:45 +0100

    signup form testing + implementation. The page redirects to /welcome page after the page has been submited.

--- cypress-testing/cypress/integration/server-test.js
+++ cypress-testing/cypress/integration/server-test.js
@@ -7,12 +7,24 @@ it('index page with message appears', () => {
   cy.visit('/').contains('Welcome to my site');
 });
 
-it('working /about route', () => {
+it('working "/about" route', () => {
   cy.visit('/about');
+  cy.url().get('h1').contains('About this site');
   // testing the <title> tag
   cy.title().should('eq', 'About this site');
 });
 
-it('working /sign-up route', () => {
+it('working "/sign-up" route', () => {
+  cy.visit('/');
+  cy.get('nav').find('a').contains('Sign up').click();
+  cy.url().should('include', 'sign-up');
+  cy.get('h1').contains('Sign up');
+});
+
+it('/sign-up page with a form containing email/password inputs', () => {
   cy.visit('/sign-up');
+  cy.get('form').find('input[name="email"]').type('elenamarinaki.em@gmail.com');
+  cy.get('form').find('input[name="password"]').type('12345');
+  cy.get('form').find('button').click();
+  cy.url().should('include', '/welcome');
 });
--- cypress-testing/server.js
+++ cypress-testing/server.js
@@ -3,15 +3,35 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send('<h1>Welcome to my site</h1>');
+  res.send(`
+  <h1>Welcome to my site</h1>
+  <nav>
+    <a href="/about">About</a>
+    <a href="/sign-up">Sign up</a>
+  </nav>
+  `);
 });
 
 server.get('/about', (req, res) => {
-  res.send('<title>About this site</title>');
+  res.send(`
+  <title>About this site</title>
+  <h1>About this site</h1>
+  `);
 });
 
 server.get('/sign-up', (req, res) => {
-  res.send('');
+  res.send(`
+    <h1>Sign up</h1>
+      <form method=POST>
+        <input name='email'/>
+        <input name='password'/>
+        <button>Submit</button>
+      </form>
+  `);
+});
+
+server.post('/sign-up', (req, res) => {
+  res.redirect('/welcome');
 });
 
 const PORT = 3333;
