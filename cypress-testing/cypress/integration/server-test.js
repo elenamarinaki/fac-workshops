6287bfef7b63e3e65023c6b9a3eb7670d24506c6
Elena <rhapsodyenmauve@gmail.com>
2021-09-28 20:11:28 +0100

    localhost at cypress.json

--- cypress-testing/cypress/integration/server-test.js
+++ cypress-testing/cypress/integration/server-test.js
@@ -1,10 +1,9 @@
-// const { it } = require("mocha");
-
+/* eslint-disable no-undef */
 it('can run a test', () => {
   assert.equal(1, 1);
 });
 
 it('index page with message appears', () => {
-  cy.visit('http://localhost:3333/').contains('Welcome to my site');
-  cy.title().should('eq', 'Welcome to my site');
+  cy.visit('/').contains('Welcome to my site');
+  // cy.title().should('eq', 'Welcome to my site');
 });
