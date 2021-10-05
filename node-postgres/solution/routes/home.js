20712321650ec1ba7bcfeeca73f2e1a86f14a297
Elena <rhapsodyenmauve@gmail.com>
2021-10-05 03:13:54 +0100

    safely handling user info

--- node-postgres/solution/routes/home.js
+++ node-postgres/solution/routes/home.js
@@ -3,6 +3,7 @@ const db = require('../database/connection.js');
 function get(request, response) {
   db.query(/*sql*/ `SELECT username FROM users`).then((result) => {
     const users = result.rows;
+    console.log(`these are ROWS: ${users}`);
     const userList = users.map((user) => `<li>${user.username}</li>`).join('');
     response.send(`<ul>${userList}</ul>`);
   });
