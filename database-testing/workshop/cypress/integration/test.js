b91c44844e5168a605ee7080a30cf2e8da1b6101
Elena <rhapsodyenmauve@gmail.com>
2021-10-05 12:12:34 +0100

    testing adding new user

--- database-testing/workshop/cypress/integration/test.js
+++ database-testing/workshop/cypress/integration/test.js
@@ -1,7 +1,31 @@
-beforeEach(() => {
-  cy.task('resetDb');
+// beforeEach(() => {
+//   cy.task('resetDb');
+// });
+
+// it('hi', () => {
+//   assert.equal(1, 1);
+// });
+
+describe('list of users at /', () => {
+  beforeEach(() => {
+    cy.task('resetDb');
+  });
+  it('displays a list of users', () => {
+    cy.visit('/');
+  });
 });
 
-it('hi', () => {
-  assert.equal(1, 1);
+describe('create user', () => {
+  beforeEach(() => {
+    cy.task('resetDb');
+  });
+  it('create user from /create-user', () => {
+    cy.visit('/');
+    cy.contains('New user').click();
+    cy.url().should('include', '/users/create');
+    cy.get('form').find("input[name='username']").type('elena');
+    cy.get('form').find("input[name='age']").type('32');
+    cy.get('form').find("input[name='location']").type('london');
+    cy.get('form').find('button').click();
+  });
 });
