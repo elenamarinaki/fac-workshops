28a2b69ef33fa30b60bab1eb165507e9baffb1ff
Elena <rhapsodyenmauve@gmail.com>
2021-10-18 16:31:03 +0100

    this is a test

---
+++ creating-promises/workshop/challenge-two.js
@@ -0,0 +1,96 @@
+const { resolve } = require('cypress/types/bluebird');
+const fs = require('fs');
+const { reject } = require('lodash');
+const path = require('path');
+
+/*
+ *
+ *
+ * CHALLENGE 2
+ *
+ *
+ */
+
+function readFilePromise(filePath) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(filePath, (error, contents) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(contents);
+      }
+    });
+  });
+
+  return promise;
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * TESTS
+ * don't change these!
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+const test = require('tape');
+
+test('readFilePromise returns a promise', (t) => {
+  const testPath = path.join(__dirname, 'test.txt');
+  const result = readFilePromise(testPath);
+  t.true(
+    result instanceof Promise,
+    'readFilePromise should return a Promise object'
+  );
+  if (result) {
+    result.then((contents) => {
+      t.equal(
+        contents.toString(),
+        'hello',
+        `readFilePromise("./test/txt") should be should be 'hello', received '${contents}'`
+      );
+      t.end();
+    });
+  } else {
+    t.end();
+  }
+});
+
+test('readFilePromise rejects if an error occurs', (t) => {
+  const result = readFilePromise('notReal.psd');
+  if (result) {
+    result
+      .then(() => {
+        t.fail('Nonexistent file should cause readFilePromise to reject');
+        t.end();
+      })
+      .catch((error) => {
+        t.ok(
+          error instanceof Error,
+          'readFilePromise should reject with an Error object'
+        );
+        t.equal(
+          error.message,
+          "ENOENT: no such file or directory, open 'notReal.psd'",
+          `Error message should be "ENOENT: no such file or directory, open 'notReal.psd'", received "${error.message}"`
+        );
+        t.end();
+      });
+  } else {
+    t.end();
+  }
+});
