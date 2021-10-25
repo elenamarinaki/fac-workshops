6a5a33691c1ad6903ddade69880a964dd6d782e2
Elena <rhapsodyenmauve@gmail.com>
2021-10-25 12:11:18 +0100

    ES modules workshop

---
+++ es-modules/workshop/calculate.js
@@ -0,0 +1,19 @@
+import { add, subtract, multiply, divide } from './math.js';
+
+
+function calculate(a, b, sign) {
+  switch (sign) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      throw new Error(`Sign '${sign}' not supported`);
+  }
+}
+
+export default calculate;
